ce19bad1cbad0bd2ce052abb4024438abe17d6ac
kewal ukunde <kewalukunde@gmail.com>
2018-02-12 16:18:35 +0530

    new update

--- text_indexer.c
+++ text_indexer.c
@@ -1,207 +1,206 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "../include/text_indexer.h"
 
 /*create data base function is create a list of structurs 
- from A-Z there we can store index and pointer for word info*/
+  from A-Z there we can store index and pointer for word info*/
 int create_data_base(data_base_t **head)
 {
-    data_base_t *ptr;
-    int i = 2,idx = 0;
-    char *argv_op;
+	data_base_t *ptr;
+	int i = 2,idx = 0;
+	char *argv_op;
 
-    ptr = calloc(1,sizeof(data_base_t)); 
-    *head = ptr;
+	ptr = calloc(1,sizeof(data_base_t)); 
+	*head = ptr;
 
-    while (1)
-    {
-	ptr -> index = idx;
-
-	ptr -> data = NULL;
-
-	if (MAX_IDX == idx++)
+	while (1)
 	{
-	    ptr -> d_next = NULL; 
-	    break;
-	}
-	else
-	{
-	    ptr -> d_next = calloc(1,sizeof(data_base_t)); 
-	}
+		ptr -> index = idx;
 
-	ptr = ptr -> d_next;
-    }
-    return 0;
+		ptr -> data = NULL;
+
+		if (MAX_IDX == idx++)
+		{
+			ptr -> d_next = NULL; 
+			break;
+		}
+		else
+		{
+			ptr -> d_next = calloc(1,sizeof(data_base_t)); 
+		}
+
+		ptr = ptr -> d_next;
+	}
+	return 0;
 }
 
 /*this function display data of back_up file*/
 int display_data_base(data_base_t **head,FILE *fp1)
 {
-    data_base_t *ptr;
-    char char_idx = '`';
-    file_i *temp1;
-    word_i *temp2;
+	data_base_t *ptr;
+	char char_idx = '`';
+	file_i *temp1;
+	word_i *temp2;
 
-    re_create_data_base(head,fp1);
+	re_create_data_base(head,fp1);
 
-    ptr = *head;
-    
-    if (ptr == NULL)
-	return 0;
-    /*from head traveling forword and printiong the info
-     if ptr->data if not NULL*/
-    while (ptr != NULL)
-    {
-	char_idx++;
-	if(ptr -> data != NULL)
+	ptr = *head;
+
+	if (ptr == NULL)
+		return 0;
+	/*from head traveling forword and printiong the info
+	  if ptr->data if not NULL*/
+	while (ptr != NULL)
 	{
-	    printf("%c] : \n",char_idx);
-
-	    printf("==========================================================\n");
-	    temp1 = ptr -> data;
-	    temp2 = temp1 -> file_table;
-	    while (temp1)
-	    {
-		printf("word        : %s\n",temp1 -> word);
-		printf("File count  : %d\n",temp1 -> file_count);
-
-		printf("------------------------------\n");
-		printf("%-15s  %-12s\n","File name","word count");
-		printf("------------------------------\n");
-		temp2 = temp1 -> file_table;
-		/*loop for mulltiple file*/
-		while (temp2)
+		char_idx++;
+		if(ptr -> data != NULL)
 		{
-		    printf("%-15s  %6d\n",temp2 -> file_name,temp2 -> word_count);
-		    temp2 = temp2 -> next;
+			printf("%c] : \n",char_idx);
+
+			printf("==========================================================\n");
+			temp1 = ptr -> data;
+			temp2 = temp1 -> file_table;
+			while (temp1)
+			{
+				printf("word        : %s\n",temp1 -> word);
+				printf("File count  : %d\n",temp1 -> file_count);
+
+				printf("------------------------------\n");
+				printf("%-15s  %-12s\n","File name","word count");
+				printf("------------------------------\n");
+				temp2 = temp1 -> file_table;
+				/*loop for mulltiple file*/
+				while (temp2)
+				{
+					printf("%-15s  %6d\n",temp2 -> file_name,temp2 -> word_count);
+					temp2 = temp2 -> next;
+				}
+				printf("------------------------------\n\n");
+				temp1 = temp1 -> f_next;
+			}
+			printf("==========================================================\n\n");
 		}
-		printf("------------------------------\n\n");
-		temp1 = temp1 -> f_next;
-	    }
-	    printf("==========================================================\n\n");
+		ptr = ptr -> d_next;
 	}
-	ptr = ptr -> d_next;
-    }
-    return 1;
+	return 1;
 }
 
 /*this function insert the word info into the data base*/
 int insert_data_base(data_base_t **head,char *str,char *argv_file)
 { 
-    data_base_t *ptr;
-    file_i *temp;
-    word_i *w_i;
-    int st;
+	data_base_t *ptr;
+	file_i *temp;
+	word_i *w_i;
+	int st;
 
-    char ch = str[0];
+	char ch = str[0];
 
-    int num = index_conv(ch);
+	int num = index_conv(ch);
 
-    ptr = *head;
+	ptr = *head;
 
-    if (ptr == NULL)
-	return 0;
+	if (ptr == NULL)
+		return 0;
 
-    /*searching for index*/
-    while (ptr -> index != num)
-    {
-	ptr = ptr -> d_next;
-    }
-
-    /*loop compare word and file present or not
-     if present then go for file, if file present go for word count*/
-    if(ptr -> data == NULL)//first time when ptr->data is NULL
-    {
-	ptr -> data = calloc(1, sizeof(file_i));
-	strcpy((ptr -> data)-> word, str);
-	(ptr -> data) -> file_count = 1;
-
-	(ptr -> data -> file_table) = calloc(1, sizeof(word_i));
-	strcpy((ptr -> data -> file_table) -> file_name,argv_file);
-	ptr -> data -> file_table -> word_count = 1;
-	ptr -> data -> file_table -> next = NULL;
-
-	(ptr -> data) -> f_next = NULL;
-    }
-    else
-    {
-	temp = ptr -> data;
-	w_i = temp -> file_table;
-
-	/*compare for word*/
-	while((st = strcmp( temp -> word, str)) && (temp -> f_next != NULL))
+	/*searching for index*/
+	while (ptr -> index != num)
 	{
-	    temp = temp -> f_next;
+		ptr = ptr -> d_next;
 	}
 
-	if (st == 0)
+	/*loop compare word and file present or not
+	  if present then go for file, if file present go for word count*/
+	if(ptr -> data == NULL)//first time when ptr->data is NULL
 	{
-	    /*compare for file name*/
-	    while ((st = strcmp(w_i -> file_name,argv_file)) && (w_i -> next != NULL))
-	    {
-		w_i = w_i -> next;
-	    }
+		ptr -> data = calloc(1, sizeof(file_i));
+		strcpy((ptr -> data)-> word, str);
+		(ptr -> data) -> file_count = 1;
 
-	    if(!st) 
-	    {
-		(w_i -> word_count)++;
-	    }
-	    else if (w_i -> next == NULL)
-	    {
-		(temp -> file_count)++;
-		w_i -> next = calloc(1, sizeof(word_i));
-		strcpy(w_i -> next -> file_name,argv_file);
-		( w_i -> next -> word_count) = 1;
-		w_i -> next -> next = NULL;
-	    }
+		(ptr -> data -> file_table) = calloc(1, sizeof(word_i));
+		strcpy((ptr -> data -> file_table) -> file_name,argv_file);
+		ptr -> data -> file_table -> word_count = 1;
+		ptr -> data -> file_table -> next = NULL;
+
+		(ptr -> data) -> f_next = NULL;
 	}
 	else
 	{
-	    temp -> f_next = calloc(1, sizeof(file_i));
-	    strcpy((temp -> f_next) -> word, str);
-	    (temp -> f_next -> file_count)++;
+		temp = ptr -> data;
+		w_i = temp -> file_table;
 
-	    temp = temp -> f_next;
+		/*compare for word*/
+		while((st = strcmp( temp -> word, str)) && (temp -> f_next != NULL))
+		{
+			temp = temp -> f_next;
+		}
 
-	    temp -> file_table = calloc(1, sizeof(word_i));
-	    strcpy(temp -> file_table-> file_name,argv_file);
-	    ( temp -> file_table -> word_count) = 1;
-	    temp -> file_table -> next = NULL;
+		if (st == 0)
+		{
+			/*compare for file name*/
+			while ((st = strcmp(w_i -> file_name,argv_file)) && (w_i -> next != NULL))
+			{
+				w_i = w_i -> next;
+			}
 
-	    temp -> f_next = NULL;
+			if(!st) 
+			{
+				(w_i -> word_count)++;
+			}
+			else if (w_i -> next == NULL)
+			{
+				(temp -> file_count)++;
+				w_i -> next = calloc(1, sizeof(word_i));
+				strcpy(w_i -> next -> file_name,argv_file);
+				( w_i -> next -> word_count) = 1;
+				w_i -> next -> next = NULL;
+			}
+		}
+		else
+		{
+			temp -> f_next = calloc(1, sizeof(file_i));
+			strcpy((temp -> f_next) -> word, str);
+			(temp -> f_next -> file_count)++;
+
+			temp = temp -> f_next;
+
+			temp -> file_table = calloc(1, sizeof(word_i));
+			strcpy(temp -> file_table-> file_name,argv_file);
+			( temp -> file_table -> word_count) = 1;
+			temp -> file_table -> next = NULL;
+
+			temp -> f_next = NULL;
+		}
 	}
-    }
 
-    return 1;
+	return 1;
 }
 
 /*update data base recives new file and update the data in data base*/
 int update_data_base(data_base_t **head,FILE *n_file,FILE *b_file,char *n_f_name)
 {
-    char word[30];
-    int i,j,f_count,w_count;
+	char word[30];
+	int i,j,f_count,w_count;
 
-    if (*head == NULL)
-	return 0;
-    re_create_data_base(head,b_file);
-    
-    while (fscanf(n_file,"%s",word) != EOF)
-	insert_data_base(head,word,n_f_name);
+	if (*head == NULL)
+		return 0;
+	re_create_data_base(head,b_file);
 
-    return 1;
+	while (fscanf(n_file,"%s",word) != EOF)
+		insert_data_base(head,word,n_f_name);
+
+	return 1;
 }
 
 /*this function forms the data base form the backup file*/
 int re_create_data_base(data_base_t **head,FILE *b_file)
 {
-    char *t = ":";
-    char line[100],word[30],f_name[30];
-    int i,j,f_count,w_count;
+	char *t = ":";
+	char line[100],word[30],f_name[30];
+	int i,j,f_count,w_count;
 
-   if (*head == NULL)
-	return 0;
-    
+	if (*head == NULL)
+		return 0;
+
     while (fscanf(b_file,"%s",line) != EOF)
     {
 	strcpy(word,strtok(line,t));
--- text_indexer_main.c
+++ text_indexer_main.c
@@ -1,214 +1,165 @@
 #include <stdio.h>
 #include <string.h>
-#include "../include/text_indexer.h"
+#include "text_indexer.h"
+#include "text_indexer.c"
 #include <unistd.h>
 
 int main(int argc,char *argv[])
 {
-    FILE *fp1,*fp2;
-    char buff[20],word[30],b_file[30];
-    int choice,i = 1,z = 1,status = 0,b_file_state = 0;
-    
-    data_base_t *head = NULL;
-    file_i *ptr;
-    
-    /*create data base basic list a-z*/
-    create_data_base(&head);
+	FILE *fp1,*fp2;
+	char buff[20],word[30],b_file[30];
+	int choice,i = 1,z = 1, status;
 
-    printf("\n\t\t\t\t%-25s\n","TEAXT INDEXER");
-    printf("\n%-25s\t%-25s\t%-25s\n","1. create data base","2. Search data","3. Display data base");
-    printf("%-25s\t%-25s\t%-25s\n","4. Update data base","5. Save data base","6. Exit");
+	data_base_t *head = NULL;
+	file_i *ptr;
 
-    do
-    {
-	printf("\nEnter a choice : ");
-	scanf("%d",&choice);
-	/*switch case for opration*/
-	switch(choice)
+	strcpy(b_file, "back.txt");
+
+	/*create data base basic list a-z*/
+	create_data_base(&head);
+
+	printf("\n\t\t\t\t%-25s\n","TEAXT INDEXER");
+	printf("\n%-25s\t%-25s\t%-25s\n","1. create data base","2. Search data","3. Display data base");
+	printf("%-25s\t%-25s\t%-25s\n","4. Update data base","5. Save data base","6. Exit");
+
+	do
 	{
-	    /*create data base for passed file*/
-	    case 1: printf("\n# create data base \n");
+		printf("\nEnter a choice : ");
+		scanf("%d",&choice);
+		/*switch case for opration*/
+		switch(choice)
+		{
+			/*create data base for passed file*/
+			case 1: printf("\n# create data base \n");
 
-		    if(argc > 1)
-		    {
-			/*conditions for options*/
-			while (argv[i] != NULL)
-			{
-			    if ((!strcmp(argv[i],"-o")) || (!strcmp(argv[i],"-c")))
-				status++;
-			    else if(status == 2)
-				break;
-			    i++;
-			}
-			if (status != 2)
-			{
-			    printf("NOT enouph option passed !!\n");
-			    return 0;
-			}
-			i = 1;
-
-			/*file open loop*/
-			while (argv[i] != NULL)
-			{
-			    if (!strcmp(argv[i],"-o"))
-			    {
-				strcpy(b_file,argv[i+1]);
-				break;
-			    }
-			    else if (strcmp(argv[i],"-c"))
-			    {    
-				if ((fp1 = fopen(argv[i],"r")) == NULL)
-				{	
-				    printf("File can not be open!!\n");
-				    return 0;
-				}
-				while (fscanf(fp1,"%s",buff) != EOF)
-				    insert_data_base(&head,buff,argv[i]);
-				fclose(fp1);
-			    }
-			    i++;
-			} 
-		    }
-		    /*if no command line argument passsed*/
-		    else if ((argc < 2) && (b_file_state != 1))
-		    {
-		        b_file_state = 1;
-			printf("Enter new file name :");
-			scanf("%s",word);
-			printf("Enter backup file name :");
-			scanf("%s",b_file);
-			if ((fp1 = fopen(word,"r")) == NULL)
-			{	
-			    printf("File can not be open!!\n");
-			    return 0;
-			}
-			while (fscanf(fp1,"%s",buff) != EOF)
-			    insert_data_base(&head,buff,word);
-			fclose(fp1);
-
-		    }
-		    __fpurge(stdin);
-		    break;
-		
-	    case 2:/*data search opration for perticular word from back up file*/
-		    printf("\n# Search data \n");
-
-		    if ((argc < 2) && (b_file_state != 1))
-		    {
-			b_file_state = 1;
-			printf("Enter backup file name :");
-			scanf("%s",b_file);
-		    }
-		   
-		    if ((fp1 = fopen(b_file,"r")) == NULL)
-		    {	
-			printf("File can not be open!!\n");
-			return 0;
-		    }
-		    printf("\nEnter word for search: ");
-		    scanf("%s",word);
-
-		    status = search_data(&head,fp1,word);
-
-		    if (!status)
-			printf("\n# NO data base found !!\n");
+					if(argc > 1)
+					{
+						if ((fp1 = fopen(argv[i],"r")) == NULL)
+						{	
+							printf("File can not be open!!\n");
+							return 0;
+						}
+						while (fscanf(fp1,"%s",buff) != EOF)
+							insert_data_base(&head,buff,argv[i]);
+						fclose(fp1);
+					}
+					/*if no command line argument passsed*/
+					else
+					{
+						printf("Enter new file name :");
+						scanf("%s",word);
 			
-		    fclose(fp1);
+						if ((fp1 = fopen(word,"r")) == NULL)
+						{	
+							printf("File can not be open!!\n");
+							return 0;
+						}
+						while (fscanf(fp1,"%s",buff) != EOF)
+							insert_data_base(&head,buff,word);
+						fclose(fp1);
 
-		    __fpurge(stdin);
-		    break;
+					}
+					__fpurge(stdin);
+					break;
 
-	    case 3:/*display all the data base saved in back up*/
-		    printf("\n# Display data base\n");
+			case 2:/*data search opration for perticular word from back up file*/
+					printf("\n# Search data \n");
 
-		    if ((argc < 2) && (b_file_state != 1))
-		    {
-			b_file_state = 1;
-			printf("Enter backup file name :");
-			scanf("%s",b_file);
-		    }
+					if ((fp1 = fopen(b_file,"r")) == NULL)
+					{	
+						printf("File can not be open!!\n");
+						return 0;
+					}
+					printf("\nEnter word for search: ");
+					scanf("%s",word);
 
-		    if ((fp1 = fopen(b_file,"r")) == NULL)
-		    {	
-			printf("File can not be open!!\n");
-			return 0;
-		    }
+					status = search_data(&head,fp1,word);
 
-		    status = display_data_base(&head,fp1);
+					if (!status)
+						printf("\n# NO data base found !!\n");
 
-		    if (!status)
-			printf("\n# NO data base found !!\n");
+					fclose(fp1);
 
-		    fclose(fp1);
-		    __fpurge(stdin);
-		    break;
-#if 1
-	    case 4:/*update data base by new file*/
-		    printf("\n# Update data base \n");
+					__fpurge(stdin);
+					break;
 
-		    printf("Enter new file name :");
-		    scanf("%s",word);
+			case 3:/*display all the data base saved in back up*/
+					printf("\n# Display data base\n");
 
-		    if ((argc < 2) && (b_file_state != 1))
-		    {
-			b_file_state = 1;
-			printf("Enter backup file name :");
-			scanf("%s",b_file);
-		    }
+					if ((fp1 = fopen(b_file,"r")) == NULL)
+					{	
+						printf("File can not be open!!\n");
+						return 0;
+					}
 
-		    if ((fp1 = fopen(word,"r")) == NULL)
-		    {	
-			printf("File can not be open!!\n");
-			return 0;
-		    }
+					status = display_data_base(&head,fp1);
 
-		    if ((fp2 = fopen(b_file,"r+")) == NULL)
-		    {	
-			printf("File can not be open!!\n");
-			return 0;
-		    }
+					if (!status)
+						printf("\n# NO data base found !!\n");
 
-		    status = update_data_base(&head,fp1,fp2,word);
+					fclose(fp1);
+					__fpurge(stdin);
+					break;
 
-		    if (status)
-			printf("\n# Data base updated.\n");
-		    else
-			printf("\n# NO data base found !!\n");
+			case 4:/*update data base by new file*/
+					printf("\n# Update data base \n");
 
-		    fclose(fp1);
-		    fclose(fp2);
-		    __fpurge(stdin);
-		    break;
-#endif
-	    case 5:/*save data base after creating data base*/
-		    printf("\n# save data base\n");
+					printf("Enter new file name :");
+					scanf("%s",word);
 
-		    if ((fp1 = fopen(b_file,"w")) == NULL)
-		    {	
-			printf("File can not be open!!\n");
-			printf("pass output file!!\n");
-			return 0;
-		    }
+					if ((fp1 = fopen(word,"r")) == NULL)
+					{	
+						printf("File can not be open!!\n");
+						return 0;
+					}
 
-		    status = save_data_base(head,fp1);
-		    if(status == 1)
-			printf("File successfully saved!!\n");
-		    else
-			printf("File not saved, may be no data found!!\n");
+					if ((fp2 = fopen(b_file,"r+")) == NULL)
+					{	
+						printf("File can not be open!!\n");
+						return 0;
+					}
 
-		    fclose(fp1);
-		    __fpurge(stdin);
-		    break;
+					status = update_data_base(&head,fp1,fp2,word);
 
-	    case 6: z = 0;
-		    break;
+					if (status)
+						printf("\n# Data base updated.\n");
+					else
+						printf("\n# NO data base found !!\n");
 
-	    default:/*for wrong choice*/
-		    printf("\nYou entered wrong choice \n");
-		    break;
-	}
-	__fpurge(stdin);
-    }while(z == 1);
+					fclose(fp1);
+					fclose(fp2);
+					__fpurge(stdin);
+					break;
 
-    return 0;
+			case 5:/*save data base after creating data base*/
+					printf("\n# save data base\n");
+
+					if ((fp1 = fopen(b_file,"w")) == NULL)
+					{	
+						printf("File can not be open!!\n");
+						printf("pass output file!!\n");
+						return 0;
+					}
+
+					status = save_data_base(head,fp1);
+					if(status == 1)
+						printf("File successfully saved!!\n");
+					else
+						printf("File not saved, may be no data found!!\n");
+
+					fclose(fp1);
+					__fpurge(stdin);
+					break;
+
+			case 6: z = 0;
+					break;
+
+			default:/*for wrong choice*/
+					printf("\nYou entered wrong choice \n");
+					break;
+		}
+		__fpurge(stdin);
+	}while(z == 1);
+
+	return 0;
 }
